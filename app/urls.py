7fae723c208f3d82bf91a27b9b912166ea362ee2
AlzyWelzy <welzyalzy@gmail.com>
2024-02-23 19:30:55 +0530

    adding new authentication and creation

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("dashboard/", views.dashboard_view, name="dashboard_view"),
     path("edit_profile/", views.edit_profile, name="edit_profile"),
-    path("signin/", views.signin_view, name="signin_view"),
     path("add_profile_picture/", views.add_profile_picture, name="add_profile_picture"),
     path("register/", views.register, name="register"),
 ]
--- core/urls.py
+++ core/urls.py
@@ -19,11 +19,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # path("", include("django.contrib.auth.urls")),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("", include("app.urls")),
-    path("", include("django.contrib.auth.urls")),
 ]
 
 if settings.DEBUG:
