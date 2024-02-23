1693ed9c077ede97300dd310f2165ca8a8e170aa
AlzyWelzy <welzyalzy@gmail.com>
2024-02-23 10:24:18 +0530

    initializtion

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "app/index.html")
