88d218092b13ee0b7a644c3a5c3005dea1db12b4
AlzyWelzy <welzyalzy@gmail.com>
2024-02-24 15:00:38 +0530

    adding new models

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class UserProfileModel(AbstractUser):
+
     email = models.EmailField(blank=True, null=True, unique=True)
     profile_picture = models.ImageField(
         upload_to="profile_picture", blank=True, null=True
@@ -15,3 +16,14 @@ class UserProfileModel(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class FoodModel(models.Model):
+    user = models.ForeignKey(UserProfileModel, on_delete=models.CASCADE)
+    food_name = models.CharField(max_length=100)
+    description = models.TextField()
+    food_type = models.CharField(max_length=100)
+    food_picture = models.ImageField(upload_to="food_picture", blank=True, null=True)
+
+    def __str__(self):
+        return self.food_name
