9f73a9e01433b5823863756e45e8b740524bee46
AlzyWelzy <welzyalzy@gmail.com>
2024-02-24 10:31:52 +0530

    email authentication added

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ from verify_email.email_handler import send_verification_email
 def activateEmail(request, user, to_email):
     messages.success(
         request,
-        f"Account successfully created for {user}. An email has been sent to {to_email} with instructions to activate the account.",
+        f"Account successfully created for {user}. An email has been sent to {to_email} with instructions to activate the account. Please check your spam folder. The link will expire in 15 minutes.",
     )
 
 
